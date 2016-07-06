498defb87d5430a488dd38b5ffd16807555f8161
raduq <raduq_88@hotmail.com>
2016-07-05 21:06:27 -0300

    angularzisse

--- src/main/java/com/raduq/shopping/ShoppingCartApplication.java
+++ src/main/java/com/raduq/shopping/ShoppingCartApplication.java
@@ -2,6 +2,7 @@ package com.raduq.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,6 +16,11 @@ import org.springframework.context.annotation.Configuration;
 @EntityScan(basePackages = "com.raduq.shopping")
 @ComponentScan
 public class ShoppingCartApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShoppingCartApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,4 @@
+angular.module('shoppingCart', ['ngResource'])
+    .controller('CartController', ['$resource', function ($resource) {
+
+    }]);
